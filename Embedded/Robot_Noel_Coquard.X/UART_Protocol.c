f009e85e239c26093b4101a38dea55d4ba567d14
IUT GEII Toulon Table 5 <touloniut@gmail.com>
2024-01-15 13:24:00 +0100

    DECODE censé marcher

--- Embedded/Robot_Noel_Coquard.X/UART_Protocol.c
+++ Embedded/Robot_Noel_Coquard.X/UART_Protocol.c
@@ -1,23 +1,17 @@
 #include <xc.h>
 #include "UART_Protocol.h"
 #include "UART.h"
-
-#define ATTENTE 0 
-#define FUNCTION_MSB 1
-#define FUNCTION_LSB 2
-#define PAYLOAD_LENGTH_MSB 3
-#define PAYLOAD_LENGTH_LSB 4
-#define PAYLOAD 5
-#define CHECKSUM 6
+#include "CB_TX1.h"
+#include "CB_RX1.h"
 
 unsigned char UartCalculateChecksum(int msgFunction, int msgPayloadLength, unsigned char* msgPayload) {
     //Fonction prenant entree la trame et sa longueur pour calculer le checksum
-    char checksum = 0;
+    unsigned char checksum = 0;
     checksum ^= 0xFE;
-    checksum ^= (char) (msgFunction >> 8);
-    checksum ^= (char) (msgFunction);
-    checksum ^= (char) (msgPayloadLength >> 8);
-    checksum ^= (char) (msgPayloadLength);
+    checksum ^= (unsigned char) (msgFunction >> 8);
+    checksum ^= (unsigned char) (msgFunction);
+    checksum ^= (unsigned char) (msgPayloadLength >> 8);
+    checksum ^= (unsigned char) (msgPayloadLength);
 
     for (int i = 0; i < msgPayloadLength; i++) {
         checksum ^= msgPayload[i];
@@ -31,13 +25,13 @@ void UartEncodeAndSendMessage(int msgFunction, int msgPayloadLength, unsigned ch
     //char b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12 ; 
 
 
-    char message[msgPayloadLength + 6];
+    unsigned char message[msgPayloadLength + 6];
     int pos = 0;
     message[pos++] = 0xFE;
-    message[pos++] = (char) (msgFunction >> 8);
-    message[pos++] = (char) msgFunction;
-    message[pos++] = (char) (msgPayloadLength >> 8);
-    message[pos++] = (char) msgPayloadLength;
+    message[pos++] = (unsigned char) (msgFunction >> 8);
+    message[pos++] = (unsigned char) msgFunction;
+    message[pos++] = (unsigned char) (msgPayloadLength >> 8);
+    message[pos++] = (unsigned char) msgPayloadLength;
 
     for (int i = 0; i < msgPayloadLength; i++) {
         message[pos++] = msgPayload[i];
@@ -59,44 +53,44 @@ void UartEncodeAndSendMessage(int msgFunction, int msgPayloadLength, unsigned ch
     b11 = message[11] ;
     b12 = message[12] ;*/
 
-
     SendMessage(message, msgPayloadLength + 6);
 }
 
+int rcvState = ATTENTE;
+int msgDecodedFunction = 0;
+int msgDecodedPayloadLength = 0;
+int msgDecodedPayloadIndex = 0;
+unsigned char msgDecodedPayload[128];
+
 void UartDecodeMessage(unsigned char c) {
     //Fonction prenant en entree un octet et servant a reconstituer les trames
-    static int rcvState = ATTENTE;
-    int msgDecodedFunction = 0;
-    int msgDecodedPayloadLength = 0;
-    char msgDecodedPayload[];
-    int msgDecodedPayloadIndex = 0;
-
     switch (rcvState) {
-        case ATTENTE :
+        case ATTENTE:
             if (c == 0xFE) {
                 rcvState = FUNCTION_MSB;
-                msgDecodedPayloadIndex = 0;
             }
+            msgDecodedPayloadLength = 0;
+            msgDecodedPayloadIndex = 0;
+            msgDecodedFunction = 0;
             break;
 
-        case FUNCTION_MSB :
+        case FUNCTION_MSB:
             msgDecodedFunction = c << 8;
             rcvState = FUNCTION_LSB;
             break;
 
-        case FUNCTION_LSB :
+        case FUNCTION_LSB:
             msgDecodedFunction |= c;
             rcvState = PAYLOAD_LENGTH_MSB;
             break;
 
-        case PAYLOAD_LENGTH_MSB :
+        case PAYLOAD_LENGTH_MSB:
             msgDecodedPayloadLength = c << 8;
             rcvState = PAYLOAD_LENGTH_LSB;
             break;
 
-        case PAYLOAD_LENGTH_LSB :
+        case PAYLOAD_LENGTH_LSB:
             msgDecodedPayloadLength |= c;
-            char msgDecodedPayload[msgDecodedPayloadLength];
             if (msgDecodedPayloadLength == 0) {
                 rcvState = CHECKSUM;
             } else if (msgDecodedPayloadLength > 1024) {
@@ -106,26 +100,21 @@ void UartDecodeMessage(unsigned char c) {
             }
             break;
 
-        case PAYLOAD :
+        case PAYLOAD:
             msgDecodedPayload[msgDecodedPayloadIndex++] = c;
             if (msgDecodedPayloadIndex >= msgDecodedPayloadLength) {
                 rcvState = CHECKSUM;
             }
             break;
 
-        case CHECKSUM :
-            char receivedChecksum = c;
-            char calculatedChecksum = UartCalculateChecksum(msgDecodedFunction, msgDecodedPayloadLength, msgDecodedPayload);
-
-            if (calculatedChecksum == receivedChecksum) {
+        case CHECKSUM:
+            if (UartCalculateChecksum(msgDecodedFunction, msgDecodedPayloadLength, msgDecodedPayload) == c) {
                 UartProcessDecodedMessage(msgDecodedFunction, msgDecodedPayloadLength, msgDecodedPayload);
-            } else {
-                //textBoxReception.Text += "\nBad CheckSum\n" ;
             }
             rcvState = ATTENTE;
             break;
 
-        default :
+        default:
             rcvState = ATTENTE;
             break;
     }
@@ -134,5 +123,21 @@ void UartDecodeMessage(unsigned char c) {
 void UartProcessDecodedMessage(int function, int payloadLength, unsigned char* payload) {
     //Fonction appelee apres le decodage pour executer l?action
     //correspondant au message recu
+    switch (function) {
+        case (int) CODE_LED:
+            if (payload[0] == 0) {
+                LED_BLANCHE = payload[1];
+            } else if (payload[0] == 1) {
+                LED_BLEUE = payload[1];
+            } else if (payload[0] == 2) {
+                LED_ORANGE = payload[1];
+            }
+            break;
 
+        case (int) CMD_ID_TEXT:
+        case (int) CMD_ID_TELEMETRE_IR:
+        case (int) CMD_ID_CONSIGNE_VITESSE:
+            break;
+
+    }
 }
--- Embedded/Robot_Noel_Coquard.X/UART_Protocol.h
+++ Embedded/Robot_Noel_Coquard.X/UART_Protocol.h
@@ -8,6 +8,22 @@
 #ifndef UART_H
 #define UART_H
 
+#define ATTENTE 0 
+#define FUNCTION_MSB 1
+#define FUNCTION_LSB 2
+#define PAYLOAD_LENGTH_MSB 3
+#define PAYLOAD_LENGTH_LSB 4
+#define PAYLOAD 5
+#define CHECKSUM 6
+
+#define CODE_LED 0x0020
+#define CODE_TELEMETRE_ED 0x0030
+#define CODE_TELEMETRE_D 0x0031
+#define CODE_TELEMETRE_C 0x0032
+#define CODE_TELEMETRE_G 0x0033
+#define CODE_TELEMETRE_EG 0x0034
+#define CODE_VITESSE 0x0040
+
 unsigned char UartCalculateChecksum(int, int, unsigned char*) ;
 
 void UartEncodeAndSendMessage(int, int, unsigned char*) ;
